2b2815d704330f3de45273e0bd8c8a9d7a455d3c
林厚忠 <hz_lin8@163.com>
2022-11-08 08:27:53 +0000

    update run_func_test.sh.
    Signed-off-by: 林厚忠 <hz_lin8@163.com>

--- run_func_test.sh
+++ run_func_test.sh
@@ -1,211 +1,133 @@
+#!/bin/bash
 set -e
 
-export ASAN_OPTIONS=halt_on_error=0;log_path=/tmp/asan.log;detect_leaks=1
+bash build.sh 2
+bash build_test.sh
 
+export ASAN_OPTIONS=log_path=./asan.log:detect_leaks=1
+export OMP_NUM_THREADS=48
 #export LD_PRELOAD=/usr/lib/gcc/aarch64-linux-gnu/10.3.1/libasan.so
 
 echo "Running functional tests"
 
 echo "test RK"
+for type in {'SINGLE','DOUBLE','COMPLEX','COMPLEX16'}
+do
+    echo "test $type"
+    ./test/func_test_RK/func_test_RK_$type.o N
+    ./test/func_test_RK/func_test_RK_$type.o E
+    echo 'test small'
+    ((time=$RANDOM%11+20))
+    echo "test time = $time"
+    for ((i=0;i<$time;i++)); do
+        ((UPLO=$RANDOM%2+1))
+        if [ $UPLO -eq 1 ]
+        then
+            UPLO=L
+        else
+            UPLO=U
+        fi
+        
+        ((N=$RANDOM%900+100))
+        ((NRHS=$RANDOM%($N-100)+100))
 
-echo 'test SINGLE'
-./test/func_test_RK/func_test_RK_SINGLE.o N
-./test/func_test_RK/func_test_RK_SINGLE.o E
-echo 'test small'
-for i in $(seq 100 100 900); do
-./test/func_test_RK/generate_func_test_RK_SINGLE.o S $i $i U
-./test/func_test_RK/func_test_RK_SINGLE.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_SINGLE.o S $i $i L
-./test/func_test_RK/func_test_RK_SINGLE.o S $i $i L
-done
-echo 'test medium'
-for i in $(seq 1000 1000 9000); do
-./test/func_test_RK/generate_func_test_RK_SINGLE.o S $i $i U
-./test/func_test_RK/func_test_RK_SINGLE.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_SINGLE.o S $i $i L
-./test/func_test_RK/func_test_RK_SINGLE.o S $i $i L
-done
-echo 'test large'
-for i in $(seq 10000 10000 40000); do
-./test/func_test_RK/generate_func_test_RK_SINGLE.o S $i $i U
-./test/func_test_RK/func_test_RK_SINGLE.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_SINGLE.o S $i $i L
-./test/func_test_RK/func_test_RK_SINGLE.o S $i $i L
-done
+        echo N=$N
+        echo NRHS=$NRHS
+        echo UPLO=$UPLO
+        ./test/func_test_RK/generate_func_test_RK_$type.o S $N $NRHS $UPLO
+        ./test/func_test_RK/func_test_RK_$type.o S $N $NRHS $UPLO
+    done
 
-echo 'test DOUBLE'
-./test/func_test_RK/func_test_RK_DOUBLE.o N
-./test/func_test_RK/func_test_RK_DOUBLE.o E
-echo 'test small'
-for i in $(seq 100 100 900); do
-./test/func_test_RK/generate_func_test_RK_DOUBLE.o S $i $i U
-./test/func_test_RK/func_test_RK_DOUBLE.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_DOUBLE.o S $i $i L
-./test/func_test_RK/func_test_RK_DOUBLE.o S $i $i L
-done
-echo 'test medium'
-for i in $(seq 1000 1000 9000); do
-./test/func_test_RK/generate_func_test_RK_DOUBLE.o S $i $i U
-./test/func_test_RK/func_test_RK_DOUBLE.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_DOUBLE.o S $i $i L
-./test/func_test_RK/func_test_RK_DOUBLE.o S $i $i L
-done
-echo 'test large'
-for i in $(seq 10000 10000 40000); do
-./test/func_test_RK/generate_func_test_RK_DOUBLE.o S $i $i U
-./test/func_test_RK/func_test_RK_DOUBLE.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_DOUBLE.o S $i $i L
-./test/func_test_RK/func_test_RK_DOUBLE.o S $i $i L
-done
+    echo 'test medium'
+    ((time=$RANDOM%11+10))
+    echo "test time = $time"
+    for ((i=0;i<$time;i++)); do
+        ((UPLO=$RANDOM%2+1))
+        if [ $UPLO -eq 1 ]
+        then
+            UPLO=L
+        else
+            UPLO=U
+        fi
+        ((N=$RANDOM%9000+1000))
+        ((NRHS=$RANDOM%($N-1000)+1000))
+        ./test/func_test_RK/generate_func_test_RK_$type.o S $N $NRHS $UPLO
+        ./test/func_test_RK/func_test_RK_$type.o S $N $NRHS $UPLO
+    done
 
-echo 'test COMPLEX'
-./test/func_test_RK/func_test_RK_COMPLEX.o N
-./test/func_test_RK/func_test_RK_COMPLEX.o E
-echo 'test small'
-for i in $(seq 100 100 900); do
-./test/func_test_RK/generate_func_test_RK_COMPLEX.o S $i $i U
-./test/func_test_RK/func_test_RK_COMPLEX.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_COMPLEX.o S $i $i L
-./test/func_test_RK/func_test_RK_COMPLEX.o S $i $i L
-done
-echo 'test medium'
-for i in $(seq 1000 1000 9000); do
-./test/func_test_RK/generate_func_test_RK_COMPLEX.o S $i $i U
-./test/func_test_RK/func_test_RK_COMPLEX.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_COMPLEX.o S $i $i L
-./test/func_test_RK/func_test_RK_COMPLEX.o S $i $i L
-done
-echo 'test large'
-for i in $(seq 10000 10000 40000); do
-./test/func_test_RK/generate_func_test_RK_COMPLEX.o S $i $i U
-./test/func_test_RK/func_test_RK_COMPLEX.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_COMPLEX.o S $i $i L
-./test/func_test_RK/func_test_RK_COMPLEX.o S $i $i L
+    echo 'test large'
+    ((time=$RANDOM%5+1))
+    echo "test time = $time"
+    for ((i=0;i<$time;i++)); do
+        if [ $UPLO -eq 1 ]
+        then
+            UPLO=L
+        else
+            UPLO=U
+        fi
+        ((N=$RANDOM%30000+10000))
+        ((NRHS=$RANDOM%($N-10000)+10000))
+        ./test/func_test_RK/generate_func_test_RK_$type.o S $N $NRHS $UPLO
+        ./test/func_test_RK/func_test_RK_$type.o S $N $NRHS $UPLO
+    done
 done
 
-echo 'test COMPLEX16'
-./test/func_test_RK/func_test_RK_COMPLEX16.o N
-./test/func_test_RK/func_test_RK_COMPLEX16.o E
-echo 'test small'
-for i in $(seq 100 100 900); do
-./test/func_test_RK/generate_func_test_RK_COMPLEX16.o S $i $i U
-./test/func_test_RK/func_test_RK_COMPLEX16.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_COMPLEX16.o S $i $i L
-./test/func_test_RK/func_test_RK_COMPLEX16.o S $i $i L
-done
-echo 'test medium'
-for i in $(seq 1000 1000 9000); do
-./test/func_test_RK/generate_func_test_RK_COMPLEX16.o S $i $i U
-./test/func_test_RK/func_test_RK_COMPLEX16.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_COMPLEX16.o S $i $i L
-./test/func_test_RK/func_test_RK_COMPLEX16.o S $i $i L
-done
-echo 'test large'
-for i in $(seq 10000 10000 40000); do
-./test/func_test_RK/generate_func_test_RK_COMPLEX16.o S $i $i U
-./test/func_test_RK/func_test_RK_COMPLEX16.o S $i $i U
-./test/func_test_RK/generate_func_test_RK_COMPLEX16.o S $i $i L
-./test/func_test_RK/func_test_RK_COMPLEX16.o S $i $i L
-done
 
 echo 'test ROOK'
 
-echo 'test SINGLE'
-./test/func_test_ROOK/func_test_ROOK_SINGLE.o N
-./test/func_test_ROOK/func_test_ROOK_SINGLE.o E
-echo 'test small'
-for i in $(seq 100 100 900); do
-./test/func_test_ROOK/generate_func_test_ROOK_SINGLE.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_SINGLE.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_SINGLE.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_SINGLE.o S $i $i L
-done
-echo 'test medium'
-for i in $(seq 1000 1000 9000); do
-./test/func_test_ROOK/generate_func_test_ROOK_SINGLE.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_SINGLE.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_SINGLE.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_SINGLE.o S $i $i L
-done
-echo 'test large'
-for i in $(seq 10000 10000 40000); do
-./test/func_test_ROOK/generate_func_test_ROOK_SINGLE.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_SINGLE.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_SINGLE.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_SINGLE.o S $i $i L
-done
+for type in {'SINGLE','DOUBLE','COMPLEX','COMPLEX16'}
+do
+    echo "test $type"
+    ./test/func_test_ROOK/func_test_ROOK_$type.o N
+    ./test/func_test_ROOK/func_test_ROOK_$type.o E
+    echo 'test small'
+    ((time=$RANDOM%11+20))
+    echo "test time = $time"
+    for ((i=0;i<$time;i++)); do
+        ((UPLO=$RANDOM%2+1))
+        if [ $UPLO -eq 1 ]
+        then
+            UPLO=L
+        else
+            UPLO=U
+        fi
+        ((N=$RANDOM%900+100))
+        ((NRHS=$RANDOM%($N-100)+100))
+        ./test/func_test_ROOK/generate_func_test_ROOK_$type.o S $N $NRHS $UPLO
+        ./test/func_test_ROOK/func_test_ROOK_$type.o S $N $NRHS $UPLO
+    done
 
-echo 'test DOUBLE'
-./test/func_test_ROOK/func_test_ROOK_DOUBLE.o N
-./test/func_test_ROOK/func_test_ROOK_DOUBLE.o E
-echo 'test small'
-for i in $(seq 100 100 900); do
-./test/func_test_ROOK/generate_func_test_ROOK_DOUBLE.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_DOUBLE.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_DOUBLE.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_DOUBLE.o S $i $i L
-done
-echo 'test medium'
-for i in $(seq 1000 1000 9000); do
-./test/func_test_ROOK/generate_func_test_ROOK_DOUBLE.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_DOUBLE.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_DOUBLE.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_DOUBLE.o S $i $i L
-done
-echo 'test large'
-for i in $(seq 10000 10000 40000); do
-./test/func_test_ROOK/generate_func_test_ROOK_DOUBLE.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_DOUBLE.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_DOUBLE.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_DOUBLE.o S $i $i L
-done
+    echo 'test medium'
+    ((time=$RANDOM%11+10))
+    echo "test time = $time"
+    for ((i=0;i<$time;i++)); do
+        ((UPLO=$RANDOM%2+1))
+        if [ $UPLO -eq 1 ]
+        then
+            UPLO=L
+        else
+            UPLO=U
+        fi
+        ((N=$RANDOM%9000+1000))
+        ((NRHS=$RANDOM%($N-1000)+1000))
+        ./test/func_test_ROOK/generate_func_test_ROOK_$type.o S $N $NRHS $UPLO
+        ./test/func_test_ROOK/func_test_ROOK_$type.o S $N $NRHS $UPLO
+    done
 
-echo 'test COMPLEX'
-./test/func_test_ROOK/func_test_ROOK_COMPLEX.o N
-./test/func_test_ROOK/func_test_ROOK_COMPLEX.o E
-echo 'test small'
-for i in $(seq 100 100 900); do
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_COMPLEX.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_COMPLEX.o S $i $i L
-done
-echo 'test medium'
-for i in $(seq 1000 1000 9000); do
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_COMPLEX.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_COMPLEX.o S $i $i L
-done
-echo 'test large'
-for i in $(seq 10000 10000 40000); do
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_COMPLEX.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_COMPLEX.o S $i $i L
-done
-
-echo 'test COMPLEX16'
-./test/func_test_ROOK/func_test_ROOK_COMPLEX16.o N
-./test/func_test_ROOK/func_test_ROOK_COMPLEX16.o E
-echo 'test small'
-for i in $(seq 100 100 900); do
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX16.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_COMPLEX16.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX16.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_COMPLEX16.o S $i $i L
-done
-echo 'test medium'
-for i in $(seq 1000 1000 9000); do
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX16.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_COMPLEX16.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX16.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_COMPLEX16.o S $i $i L
-done
-echo 'test large'
-for i in $(seq 10000 10000 40000); do
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX16.o S $i $i U
-./test/func_test_ROOK/func_test_ROOK_COMPLEX16.o S $i $i U
-./test/func_test_ROOK/generate_func_test_ROOK_COMPLEX16.o S $i $i L
-./test/func_test_ROOK/func_test_ROOK_COMPLEX16.o S $i $i L
+    echo 'test large'
+    ((time=$RANDOM%5+1))
+    echo "test time = $time"
+    echo "test time = $time"
+    for ((i=0;i<$time;i++)); do
+        if [ $UPLO -eq 1 ]
+        then
+            UPLO=L
+        else
+            UPLO=U
+        fi
+        ((N=$RANDOM%30000+10000))
+        ((NRHS=$RANDOM%($N-10000)+10000))
+        ./test/func_test_ROOK/generate_func_test_ROOK_$type.o S $N $NRHS $UPLO
+        ./test/func_test_ROOK/func_test_ROOK_$type.o S $N $NRHS $UPLO
+    done
 done
